42c911e988a6627eef16602467208b621cf4b753
fredrikscode <fredrik@rootkit.nu>
2021-10-23 21:58:36 +0200

    Environmental variables, Get-class, TunnelHealth samples

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from requests import Session
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# From .env file
+base_url = os.getenv('base_url')
+api_key = os.getenv('api_key')
+basic_auth = os.getenv('basic_auth')
+
+class Get:
+    # Base API call headers
+    def __init__(self):
+        self.apiurl = base_url
+        self.headers = {
+            "Accepts": "application/json",
+            "Authorization": "Basic " + basic_auth,
+            "Content-Type": "application/json",
+            "aw-tenant-code": api_key,
+        }
+        self.session = Session()
+        self.session.headers.update(self.headers)
+
+    # It is used to get health information for tunnel endpoint.
+    def TunnelHealth(self):
+        url = self.apiurl + "/mdm/tunnel/health"
+        r = self.session.get(url)
+        data = r.json()
+        return data
+
+    # It is used to get health information for tunnel downstream connectivity.
+    def TunnelHealthDs(self):
+        url = self.apiurl + "/mdm/tunnel/health/downstream"
+        r = self.session.get(url)
+        data = r.json()
+        return data
